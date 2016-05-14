e4ca9db70b777f3592f6cdd8aa1aa4fa7026dc7a
bo <bo_zhao@hks.harvard.edu>
2016-05-14 14:50:48 -0400

    0.2.1 update reports

--- ashcrawler/google.py
+++ ashcrawler/google.py
@@ -43,8 +43,8 @@ def ggcrawler(keyword, project, address, port):
     browser.set_page_load_timeout(TIMEOUT)
 
     client = MongoClient(address, port)
-    db_auth = client.admin
-    db_auth.authenticate('admin', 'ash123!@#')
+    db_auth = client[project]
+    db_auth.authenticate('crawler', 'ash123!@#')
     db = client[project]
     base_url = "https://www.google.com/?gws_rd=ssl#tbs=qdr:d&q="
     # base_url = "http://www.baidu.com/s?wd="
