4f20f126cb74e17e47e3b60c98b943fe3b3c0fbe
bo <bo_zhao@hks.harvard.edu>
2016-05-14 19:36:31 -0400

    0.2.5 update authentication

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@
 
 SETTINGS = {
     'project': 'philanthropy',
-    'pis': ['bo_zhao@hks.harvard.edu'],
+    'pis': ['Edward_Cunningham@hks.harvard.edu', 'David_Li@hks.harvard.edu', 'xszhang.ellie@gmail.com', 'bo_zhao@hks.harvard.edu'],
     'address': 'localhost',
     'port': 27017,
     'username': "crawler",
