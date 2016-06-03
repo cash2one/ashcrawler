3895618d584db2acff446744893ca94b29444f4f
bo <bo_zhao@hks.harvard.edu>
2016-06-03 12:18:46 +0800

    0.2.8 adding tags for weibo

--- core/utils.py
+++ core/utils.py
@@ -83,7 +83,7 @@ def del_duplicates(settings):
     # For page information
     client = MongoClient(address, port)
     db = client[project]
-    # db.authenticate(username, password)
+    db.authenticate(username, password)
 
     pages = db.pages.find({"created_at": {"$gt": start}})
 
