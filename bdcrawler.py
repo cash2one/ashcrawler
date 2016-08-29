572871d0c959a80e239cb1d7bb70eca8e67e2870
bo <bo_zhao@hks.harvard.edu>
2016-08-28 23:09:29 -0700

    0.3.1  search google for the past year.

--- bdcrawler.py
+++ bdcrawler.py
@@ -26,6 +26,11 @@ keywords = [
     '"捐赠 元 总经理" | "捐赠 元 董事长"'
     ]
 
+
+keywords = [
+    '"捐赠 元 总经理" | "捐赠 元 董事长"'
+    ]
+
 start = datetime.datetime.now()
 log(NOTICE, 'Baidu Crawler Initializing...')
 for keyword in keywords:
